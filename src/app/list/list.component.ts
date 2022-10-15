df6d10365e3e34cab11777c5cffa2349f760347e
Aadam <aadamgafar@protonmail.com>
2022-10-15 20:46:47 +0100

    Added splash graphics

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -20,16 +20,19 @@ export class ListComponent implements OnInit {
 
   ngOnInit(): void { // This method runs on initialisation (i.e. instantly)
     this.loadTask();
+    this.emptyListCheck();
   }
 
   addTask() { // Using two-way binding, appends an entered task to the array
     this.currentTasks.push(new Task(this.newTaskName, this.newTaskNotes));
     this.saveTask();
+    this.emptyListCheck();
   }
 
   delTask(taskIndex: number) { // Removes ONE specified task from array
     this.currentTasks.splice(taskIndex, 1);
     this.saveTask();
+    this.emptyListCheck();
   }
 
   saveTask() { // Saves the 'currentTasks' array in its current state
@@ -40,6 +43,17 @@ export class ListComponent implements OnInit {
     var savedData = localStorage.getItem('currentTasks');
     if (savedData != null) {
       this.currentTasks = JSON.parse(savedData);
+    }
+  }
+
+  emptyListCheck() { // Activates a graphic when the 'currentTasks' is empty
+    var emptyListMessage = document.getElementById("emptyListMessage");
+    if (emptyListMessage) {
+      if (this.currentTasks.length <= 0) {
+        emptyListMessage.hidden = false;
+      } else {
+        emptyListMessage.hidden = true;
+      }
     }
   }
 
