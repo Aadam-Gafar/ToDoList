407cad247117d48e0a70a975a782d8ba5b5ee1a1
Aadam <aadamgafar@protonmail.com>
2022-10-15 20:19:03 +0100

    Added save / load functionality

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -7,27 +7,40 @@ import { Task } from 'src/task.model';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  // Variables
+  currentTasks: Task[] = [
+    new Task("Buy groceries", "We need milk and eggs."), // Dummy task
+    new Task("Get a haircut", ""), // Dummy task
+    new Task("Visit the library", "Bring your card.") // Dummy task
+  ];
   newTaskName: string = ""; // Temporary store of new task name
   newTaskNotes: string = ""; // Temporary store of new task notes
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { // This method runs on initialisation (i.e. instantly)
+    this.loadTask();
   }
-
-  // Currently filled with dummy tasks
-  currentTasks: Task[] = [
-    new Task("Buy groceries", "We need milk and eggs."),
-    new Task("Get a haircut", ""),
-    new Task("Visit the library", "Bring your card.")
-  ]; // An array of Task objects
 
   addTask() { // Using two-way binding, appends an entered task to the array
     this.currentTasks.push(new Task(this.newTaskName, this.newTaskNotes));
+    this.saveTask();
   }
 
   delTask(taskIndex: number) { // Removes ONE specified task from array
     this.currentTasks.splice(taskIndex, 1);
+    this.saveTask();
+  }
+
+  saveTask() { // Saves the 'currentTasks' array in its current state
+    localStorage.setItem('currentTasks', JSON.stringify(this.currentTasks));
+  }
+
+  loadTask() { // Loads the 'currentTasks' array in its last saved state
+    var savedData = localStorage.getItem('currentTasks');
+    if (savedData != null) {
+      this.currentTasks = JSON.parse(savedData);
+    }
   }
 
 }
