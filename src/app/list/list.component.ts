43c752f80257ccb2562645c0037e10371ba66098
AadamGafar <Aadam.Gafar@tpnmail.co.uk>
2022-10-27 15:39:07 +0100

    Revert "GitHub Pages commit"
    This reverts commit b02d4dce478232cade3bfb860af4197500dea9b4.

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/task.model';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+  // Variables
+  currentTasks: Task[] = [
+    new Task("Buy groceries", "We need milk and eggs."), // Dummy task
+    new Task("Get a haircut", ""), // Dummy task
+    new Task("Visit the library", "Bring your card.") // Dummy task
+  ];
+  newTaskName: string = ""; // Temporary store of new task name
+  newTaskNotes: string = ""; // Temporary store of new task notes
+
+  constructor() { }
+
+  ngOnInit(): void { // This method runs on initialisation (i.e. instantly)
+    this.loadTask();
+    this.emptyListCheck();
+  }
+
+  addTask() { // Using two-way binding, appends an entered task to the array
+    if(this.newTaskName!="") {
+      console.log("Pushing");
+      this.currentTasks.push(new Task(this.newTaskName, this.newTaskNotes));
+      this.saveTask();
+      this.emptyListCheck();
+      this.newTaskName=""; // Resets input fields
+      this.newTaskNotes=""; // Resetes input fields
+    } else {
+      alert("You must enter a task name.");
+    }
+  }
+
+  delTask(taskIndex: number) { // Removes ONE specified task from array
+    this.currentTasks.splice(taskIndex, 1);
+    this.saveTask();
+    this.emptyListCheck();
+  }
+
+  saveTask() { // Saves the 'currentTasks' array in its current state
+    localStorage.setItem('currentTasks', JSON.stringify(this.currentTasks));
+  }
+
+  loadTask() { // Loads the 'currentTasks' array in its last saved state
+    var savedData = localStorage.getItem('currentTasks');
+    if (savedData != null) {
+      this.currentTasks = JSON.parse(savedData);
+    }
+  }
+
+  emptyListCheck() { // Activates a graphic when the 'currentTasks' is empty
+    var emptyListMessage = document.getElementById("emptyListMessage");
+    if (emptyListMessage) {
+      if (this.currentTasks.length <= 0) {
+        emptyListMessage.hidden = false;
+      } else {
+        emptyListMessage.hidden = true;
+      }
+    }
+  }
+}
