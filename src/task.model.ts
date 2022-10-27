b02d4dce478232cade3bfb860af4197500dea9b4
AadamGafar <Aadam.Gafar@tpnmail.co.uk>
2022-10-27 12:02:27 +0100

    GitHub Pages commit

--- src/task.model.ts
+++
@@ -1,9 +0,0 @@
-export class Task {
-    public name: string;
-    public notes: string;
-
-    constructor(nameIn: string, notesIn: string) {
-        this.name = nameIn;
-        this.notes = notesIn;
-    }
-}
